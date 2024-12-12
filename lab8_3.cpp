36e80532fd0d088ff12c5510976cee776fc5ff21
Namjimkai52 <soranathkonde@gmail.com>
2024-12-13 01:42:32 +0700

    already

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+int main(){
+    int age ;
+    int hi ;
+    int kahua ; 
+    cout << "Enter your age: ";
+    cin >> age ;
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> hi ;
+            if(hi < 100){
+                cout << "Your character = Chopper";
+            }else if(hi < 180){
+                cout << "Your character = Usopp";
+            }else {
+                cout << "Enter your bounty: ";
+                cin >> kahua ;
+                if (kahua > 1100000000){
+                    cout << "Your character = Zoro";
+                }else{
+                    cout << "Your character = Sanji";
+                }
+            } 
+    }else if(age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> kahua ; 
+        if (kahua > 500000000){
+                    cout << "Your character = Jinbe";
+                }else{
+                    cout << "Your character = Franky";
+                }
+    }else{
+        cout << "Your character = Brook";
+    }
+
+
+    return 0;
+}
+
 /*
 "Enter your age: "
 "Enter your height: "
